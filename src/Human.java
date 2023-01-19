ba44a32978c6261a157c8d0419e08480a3be640e
leo.pirringer <leo.pirringer@sz-ybbs.ac.at>
2023-01-19 19:00:36 +0100

    second commit

--- src/Human.java
+++ src/Human.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Human {
+public class Human {
+    //constructor = special method that is called when an object is created
+    //constructor has the same name as the class
+    //constructor is used to initialize the object
+
+    String name;
+    int age;
+
+    Human(String name, int age){
+        //this.name/age refers to the name of the object
+        this.name = name;
+        this.age = age;
+    }
+
+    //method
+    void eat(){
+        System.out.println(this.name + " is eating");
+    }
+
+    void walk(){
+        System.out.println(this.name + " is walking");
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
+import java.util.ArrayList;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+
+        //call the constructor
+        //create a Human
+        //instance of the Human class
+        Human human = new Human("Bob", 25);
+        //call attributes
+        System.out.println(human.name);
+        System.out.println(human.age);
+        //call the method
+        human.eat();
+        System.out.println("------------------------");
+        //create a second Human
+        //with different values
+        Human human2 = new Human("Sue", 35);
+        //call attributes
+        System.out.println(human2.name);
+        System.out.println(human2.age);
+        //call the method
+        human2.eat();
+        human2.walk();
+
+        System.out.println("------------------------");
+
+        //first Option
+        // Create an ArrayList of Strings
+        //String[] animals = {"Dog", "Cat", "Bird", "Fish"};
+
+        //second Option
+        //iterate through the elements of an array
+        ArrayList<String> animals = new ArrayList<String>();
+        animals.add("Dog");
+        animals.add("Cat");
+        animals.add("Bird");
+        animals.add("Fish");
+
+        for (String animal : animals) {
+            System.out.println(animal);
+        }
     }
 }
+
